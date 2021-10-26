3d6cd3b7ef9bb743c8f2ae8c6c84943e3a8137d4
abrarZhr <Abrar.alzhrani.ruh.js@tuwaiq.edu.sa>
2021-10-26 22:43:36 +0300

    abrar done

--- to-do/src/components/toDo.js
+++ to-do/src/components/toDo.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 function ToDoList() {
 
-    
+    const [count , setCount]=useState(0)
     return (
         <div className="todo">
             <h1>Add Your Tasks</h1>
@@ -11,6 +11,12 @@ function ToDoList() {
             <div>
                 <input type="text" name="tasks" />
                 <input type="submit" name="submit" />
+            </div>
+
+            <div>
+                <button onClick={() => setCount(count+1)}>+</button>
+                {count}
+                <button onClick={() => setCount(count-1)}>-</button>
             </div>
             
         </div>
